b21669e9c3134d7b37c379db03b7ef9c58a7cba8
Zainab Adjiet <zainab.adjiet@gmail.com>
2019-07-27 16:21:25 +0200

    single LED blinks correctly

--- Prac1/template.py
+++ Prac1/template.py
@@ -3,19 +3,29 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Zainab Adjiet
+Student Number: ADJZAI001
+Prac: Prac 1
+Date: 27/07/2019
 """
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
+
+# set up pins
+def pin_setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
 
 # Logic that you write
 def main():
-    print("write your logic here")
+    pin_setup()
 
+    GPIO.output(17, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(17, GPIO.LOW)
+    time.sleep(1)
 
 # Only run the functions if 
 if __name__ == "__main__":
@@ -27,7 +37,7 @@ if __name__ == "__main__":
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-    except e:
+    except Exception as e:
         GPIO.cleanup()
         print("Some other error occurred")
         print(e.message)
